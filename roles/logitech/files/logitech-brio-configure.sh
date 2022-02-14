6ec32b9bda9307252e8683863609cad973d9ea55
Trond Nordheim <trond@nordheim.io>
2022-02-14 14:20:32 +0100

    tweaking i3 setup

--- roles/logitech/files/logitech-brio-configure.sh
+++ roles/logitech/files/logitech-brio-configure.sh
@@ -3,5 +3,5 @@
 set -e
 
 DEVICE="$1"
-/usr/bin/v4l2-ctl -d $DEVICE --set-ctrl=zoom_absolute=160
+/usr/bin/v4l2-ctl -d $DEVICE --set-ctrl=zoom_absolute=110
 /usr/bin/v4l2-ctl -d $DEVICE --set-ctrl=pan_absolute=0
